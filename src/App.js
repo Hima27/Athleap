25295f27a291e955d97b1a64b38e30f7f954d9f6
Anjola-a <59450183+Anjola-a@users.noreply.github.com>
2021-07-24 15:41:58 -0400

    sign up bare bones

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import logo from './logo.svg'
 import './App.css'
+import { Button, Form, FormGroup, Label, Input} from 'reactstrap'
+import { FacebookLoginButton } from 'react-social-login-buttons';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit < code > src / App.js < /code> and save to reload.{' '} <
-        /p>{' '} <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React { ' ' } <
-        /a>{' '} <
-        /header>{' '} <
-        /div>
+    return ( <Form className = "signup-form" >
+        <h1 className= "text-center"><span className="font-weight-bold">Athleap</span></h1>
+        <h2 className= "text-center">Welcome</h2>
+
+        <FormGroup>
+            <Label>
+                Name
+            </Label>
+            <Input type="text" placeholder="Jane Doe"></Input>
+        </FormGroup>
+
+        <FormGroup>
+            <Label>
+                Email
+            </Label>
+            <Input type="email" placeholder="jane.doe@email.com"></Input>
+        </FormGroup>
+
+        <FormGroup>
+            <Label>
+                Password
+            </Label>
+            <Input type="password" placeholder="password"></Input>
+        </FormGroup>
+        
+        </Form>
     )
 }
 
