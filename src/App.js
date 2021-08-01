d62059907de0680bce02f80fb76f07e83444eb02
Hima Patel <hima@uoguelph.ca>
2021-08-01 15:53:30 -0400

    added background and title

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
+import logo from'./logo.png'
 
-function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit < code > src / App.js < /code> and save to reload.{' '} <
-        /p>{' '} <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React { ' ' } <
-        /a>{' '} <
-        /header>{' '} <
-        /div>
-    )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+            {/* add link for logo to refresh page once uyrl is added */}
+            <div className="logo-div">
+                <img className="logo" src={logo} alt="Atleap Logo"></img>
+            </div>
+            <div className="title-div">
+                <h1 className="app-title">Atleap</h1>
+            </div>
+            <div className="subtitle-div">
+                <h3 className="app-subtitle">Born to compete</h3>
+            </div>
+        
+            
+            {/* <button className="about-btn">About</button> */}
+        </div>
+
+        <body className="body-home">
+            <h2 className="body-title-home">Connecting disabled atheltes </h2>
+            {/* <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React */}
+            {/* </a> */}
+        </body>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
