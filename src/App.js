4b940f71c974b19f9c03efbed5896039e105f34f
Anjola-a <59450183+Anjola-a@users.noreply.github.com>
2021-08-01 16:15:03 -0400

    calender

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ class App extends Component {
 
         <body className="body-home">
             <h2 className="body-title-home">Connecting disabled atheltes </h2>
+            <iframe className="Calender"  title="Calender" src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23D50000&amp;ctz=America%2FToronto&amp;src=aGltYXBhdGVsMjdAZ21haWwuY29t&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=Mm84MnRhbGxkazdyYmJsODlqNmNtZWtsdDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%23EF6C00" style={{border: 'solid 1px #777'}} width="800" height="600" frameborder="0" scrolling="no"></iframe>
+XMA Header Image
             {/* <p>
                 Edit <code>src/App.js</code> and save to reload.
             </p>
